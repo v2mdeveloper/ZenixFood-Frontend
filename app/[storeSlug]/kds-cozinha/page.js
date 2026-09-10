14cfdc3e7e0f33c4f2f75c0649d43fcb3239b658
v2mdeveloper <canoneburger@gamil.com>
2026-09-10 02:07:22 -0300

    kds importe

--- app/[storeSlug]/kds-cozinha/page.js
+++ app/[storeSlug]/kds-cozinha/page.js
@@ -1,5 +1,5 @@
 'use client';
-import KdsEngine from './../../components/kds-engine/KdsEngine';
+import KdsEngine from '../KdsEngine';
 export default function KdsCozinhaPage() {
   return <KdsEngine mode="COZINHA" />;
 }
--- app/[storeSlug]/kds-delivery/page.js
+++ app/[storeSlug]/kds-delivery/page.js
@@ -1,5 +1,5 @@
 'use client';
-import KdsEngine from './../../components/kds-engine/KdsEngine';
+import KdsEngine from '../KdsEngine';
 export default function KdsDeliveryPage() {
   return <KdsEngine mode="DELIVERY" />;
 }
