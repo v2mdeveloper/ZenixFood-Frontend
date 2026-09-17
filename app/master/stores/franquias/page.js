175fe41c563fac4fada6f543f12863173bc7ef60
v2mdeveloper <canoneburger@gamil.com>
2026-09-17 18:55:56 -0300

    ajuste franquiados

--- app/master/stores/franquias/page.js
+++ app/master/stores/franquias/page.js
@@ -1,4 +1,4 @@
-import MasterUsersTab from '../MasterUsersTab';
+import MasterUsersTab from './MasterUsersTab';
 
 export default function FranquiasPage() {
   return <MasterUsersTab API_URL="https://zenixfood-backend.onrender.com" />;
